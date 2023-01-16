6fbde174c9ffff469b2c38539afe01e2517e9d75
Luka <lukapodra1@gmail.com>
2023-01-16 15:37:53 +0100

    admin get own data route

--- server/routes/admin/coach/index.js
+++ server/routes/admin/coach/index.js
@@ -5,7 +5,7 @@ const generalController = require('../../../controllers/admin/general')
 
 const router = express.Router()
 
-router.get('/', async(req, res) => {
+router.get('/all', async(req, res) => {
     const tokenValidation = await generalController.verifyJWT(req.header('Authorization'))
     if(!tokenValidation) {
         res.status(400).json('JWT not valid')
@@ -18,6 +18,24 @@ router.get('/', async(req, res) => {
         return
     }
     res.status(200).json(coaches)
+})
+
+router.get('/own', async(req, res) => {
+    const tokenValidation = await generalController.verifyJWT(req.header('Authorization'))
+    if(!tokenValidation) {
+        res.status(400).json('JWT not valid')
+        return
+    }
+    const coach = await coachController.getById(tokenValidation.id)
+    const databaseConnection = await generalController.checkDatabaseConnection(coach)
+    if(!databaseConnection) {
+        res.status(500).json('Error with database')
+        return
+    }
+    if(!coach) {
+        res.status(400).json('Coach not found')
+    }
+    res.status(200).json(coach)
 })
 
 module.exports = router
