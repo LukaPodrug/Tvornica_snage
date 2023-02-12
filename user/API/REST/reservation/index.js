ccbc7f0476f2511cf6681998a36e5040dbdb3a48
Luka <lukapodra1@gmail.com>
2023-02-12 14:09:14 +0100

    change api url

--- user/API/REST/reservation/index.js
+++ user/API/REST/reservation/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 async function getActiveReservationsAPI(token) {
-    const getActiveReservationsResponse = await axios.get('https://tvornica-snage-api.onrender.com/api/user/reservation/active', {
+    const getActiveReservationsResponse = await axios.get('https://tvornica-snage-api-8xrw.onrender.com/api/user/reservation/active', {
         headers: {
             'Authorization': token
         }
@@ -10,7 +10,7 @@ async function getActiveReservationsAPI(token) {
 }
 
 async function getOwnStatisticsAPI(token) {
-    const getOwnStatisticsResponse = await axios.get('https://tvornica-snage-api.onrender.com/api/user/reservation/statistics', {
+    const getOwnStatisticsResponse = await axios.get('https://tvornica-snage-api-8xrw.onrender.com/api/user/reservation/statistics', {
         headers: {
             'Authorization': token
         }
@@ -19,7 +19,7 @@ async function getOwnStatisticsAPI(token) {
 }
 
 async function addReservationAPI(token, trainingId) {
-    const addReservationResponse = await axios.post('https://tvornica-snage-api.onrender.com/api/user/reservation', {
+    const addReservationResponse = await axios.post('https://tvornica-snage-api-8xrw.onrender.com/api/user/reservation', {
         trainingId
     },
     {
@@ -31,7 +31,7 @@ async function addReservationAPI(token, trainingId) {
 }
 
 async function removeReservationAPI(token, trainingId) {
-    const removeReservationResponse = await axios.delete('https://tvornica-snage-api.onrender.com/api/user/reservation', {
+    const removeReservationResponse = await axios.delete('https://tvornica-snage-api-8xrw.onrender.com/api/user/reservation', {
         headers: {
             'Authorization': token
         },
--- user/API/REST/user/index.js
+++ user/API/REST/user/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 async function getOwnDataAPI(token) {
-    const getOwnDataResponse = await axios.get('https://tvornica-snage-api.onrender.com/api/user/user', {
+    const getOwnDataResponse = await axios.get('https://tvornica-snage-api-8xrw.onrender.com/api/user/user', {
         headers: {
             'Authorization': token
         }
